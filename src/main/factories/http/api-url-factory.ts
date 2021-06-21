827481ebb7fae8adf8754d953c21f59a03b0254e
Felipe Mendes <zo.fmendes@gmail.com>
2021-06-21 00:27:49 -0300

    fix: fix api url

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,1 +1,1 @@
-export const makeApiUrl = (path: string): string => `http://dev.keymax.com.br:3002/api${path}`
+export const makeApiUrl = (path: string): string => `https://dev.keymax.com.br:3002/api${path}`
